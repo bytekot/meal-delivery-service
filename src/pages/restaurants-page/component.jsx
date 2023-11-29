9193b1f7fa856d984d96f5a938eceaf376fa9717
bytekot <bytekot@gmail.com>
2023-11-29 21:33:47 +0300

    Lecture-5 Home work
    - remove useless RestaurantTabs

--- src/pages/restaurants-page/component.jsx
+++ src/pages/restaurants-page/component.jsx
@@ -3,19 +3,22 @@ import { useState } from 'react'
 import { RestaurantTabs } from '../../components/restaurant-tabs/component'
 import { Restaurant } from '../../components/restaurant/component'
 
+import { Tabs } from '../../components/tabs/component'
+
 import styles from './styles.module.scss'
 
 export const RestaurantsPage = ({ restaurants }) => {
-    const [restaurantId, setRestaurantId] = useState()
-    const restaurant = restaurants.find(({ id }) => restaurantId && id === restaurantId)
+    const [restaurantIndex, setRestaurantIndex] = useState(0)
+
+    console.log(restaurantIndex)
 
     return (
         <div className={styles.restaurantsPage}>
-            <RestaurantTabs
-                restaurants={restaurants.map(({ id, name }) => ({ id, name }))}
-                onTabSelect={setRestaurantId}
+            <Tabs
+                tabs={restaurants.map(({ name }) => name)}
+                onTabSelect={setRestaurantIndex}
             />
-            <Restaurant restaurant={restaurant} />
+            <Restaurant restaurant={restaurants[restaurantIndex]} />
         </div>
     )
 }
