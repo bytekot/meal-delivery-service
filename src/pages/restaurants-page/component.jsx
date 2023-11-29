ed292a09adbb2242f1cb94f1d1fc017791dbefdb
bytekot <bytekot@gmail.com>
2023-11-29 21:36:38 +0300

    Lecture-5 Home work
    - fix import

--- src/pages/restaurants-page/component.jsx
+++ src/pages/restaurants-page/component.jsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
 
-import { RestaurantTabs } from '../../components/restaurant-tabs/component'
-import { Restaurant } from '../../components/restaurant/component'
-
 import { Tabs } from '../../components/tabs/component'
+import { Restaurant } from '../../components/restaurant/component'
 
 import styles from './styles.module.scss'
 
 export const RestaurantsPage = ({ restaurants }) => {
     const [restaurantIndex, setRestaurantIndex] = useState(0)
-
-    console.log(restaurantIndex)
 
     return (
         <div className={styles.restaurantsPage}>
