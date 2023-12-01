091753557ea7ee2814e5d6b5b0d24da7c0cf4436
bytekot <bytekot@gmail.com>
2023-12-01 15:08:48 +0300

    Lecture-6 Home work
    - add buttons

--- src/components/header/component.jsx
+++ src/components/header/component.jsx
@@ -1,7 +1,14 @@
+import { Button } from '../button/component'
+
+import { BUTTON_TYPES } from '../../constants/components'
+
 import styles from './styles.module.scss'
 
 import classNames from 'classnames'
 
 export const Header = ({ className }) => (
-    <div className={classNames(styles.header, className)} />
+    <div className={classNames(styles.header, className)}>
+        <Button className={styles.button} type={BUTTON_TYPES.SECONDARY}>Toggle theme</Button>
+        <Button className={styles.button}>Order</Button>
+    </div>
 )
