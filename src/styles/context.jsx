cc0950b33f7b910a9dae3dc395d6473823022445
bytekot <bytekot@gmail.com>
2023-12-04 15:16:36 +0300

    Lecture-6 Home work
    - improve theme context

--- src/styles/context.jsx
+++ src/styles/context.jsx
@@ -1,8 +1,8 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useMemo } from 'react'
 import { THEMES } from '../constants/components'
 
 const INITIAL_STATE = {
-    theme: THEMES.LIGHT,
+    theme: matchMedia('(prefers-color-scheme: dark)').matches ? THEMES.DARK : THEMES.LIGHT,
 }
 
 export const ThemeContext = createContext(INITIAL_STATE)
@@ -10,8 +10,10 @@ export const ThemeContext = createContext(INITIAL_STATE)
 export const ThemeProvider = ({ children }) => {
     const [theme, setTheme] = useState(INITIAL_STATE.theme)
 
+    const value = useMemo(() => ({ theme, setTheme }), [theme])
+
     return (
-        <ThemeContext.Provider value={{ theme, setTheme }}>
+        <ThemeContext.Provider value={value}>
             {children}
         </ThemeContext.Provider>
     )
